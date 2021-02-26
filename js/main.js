81662c0c0d96d3586116be957daefed351288fd6
Dohyeon Han <limited.hyeon@gmail.com>
2021-02-26 23:40:57 +0900

    resize

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
 const heart = document.querySelector('.heart_btn');
+const container = document.querySelector('#container');
+const sidebox = document.querySelector('.side_box');
 
 heart.addEventListener('click',function (){
     console.log('hit');
-    heart.classList.toggle('on');
+    heart.classList.toggle('on');//좋아요 껃다 켜기
 })
+
+function resizeFunc(){
+     let calWidth = window.innerWidth;
+     sidebox.style.left = calWidth * 0.5 +164 + "px";
+}   
+
+window.onload = function(){
+    resizeFunc();
+    window.addEventListener('resize',function(){
+        resizeFunc();
+    })
+}
+
+function scrollFunc(){
+    console.log(pageYOffset);
+    if(pageYOffset >= 10){//10보다 크면
+        container.classList.add('on');//id container+on
+        sidebox.classList.add('on');
+        
+    }
+    else{
+        container.classList.remove('on');
+        sidebox.classList.remove('on');
+        //sidebox.removeAttribute('style');
+    }
+}
+
+
+window.addEventListener('scroll', scrollFunc);
 
