fa29e8818a68c4de44af91c9ec6962f478ce068c
Dohyeon Han <limited.hyeon@gmail.com>
2021-02-27 18:56:26 +0900

    front

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 const heart = document.querySelector('.heart_btn');
 const container = document.querySelector('#container');
 const sidebox = document.querySelector('.side_box');
+const delegation = document.querySelector('.contents_box');
 
-heart.addEventListener('click',function (){
-    console.log('hit');
-    heart.classList.toggle('on');//좋아요 껃다 켜기
-})
+
+
+function delegationFunc(e){
+    let elem = e.target;
+    //console.log(elem);
+
+    if(elem.matches('[data-name="heartbeat"]')){
+        console.log('하트');
+    }
+    else if(elem.matches('[data-name="bookmart"]')){
+        console.log('북마크');
+    }
+    else if(elem.matches('[data-name="share"]')){
+        console.log('공유');
+    }
+    elem.classList.toggle('on');
+}
 
 function resizeFunc(){
      let calWidth = window.innerWidth;
      sidebox.style.left = calWidth * 0.5 +164 + "px";
 }   
+
+
+function scrollFunc(){
+    if(pageYOffset >= 10){//10보다 크면
+        container.classList.add('on');//id container+on
+        if(sidebox)
+            sidebox.classList.add('on');
+        
+    }
+    else{
+        container.classList.remove('on');
+        if(sidebox)
+            sidebox.classList.remove('on');
+        //sidebox.removeAttribute('style');
+    }
+}
+
+setTimeout(function(){
+    scrollTo(0,0);
+},100);
 
 window.onload = function(){
     resizeFunc();
@@ -19,20 +53,5 @@ window.onload = function(){
     })
 }
 
-function scrollFunc(){
-    console.log(pageYOffset);
-    if(pageYOffset >= 10){//10보다 크면
-        container.classList.add('on');//id container+on
-        sidebox.classList.add('on');
-        
-    }
-    else{
-        container.classList.remove('on');
-        sidebox.classList.remove('on');
-        //sidebox.removeAttribute('style');
-    }
-}
-
-
+window.addEventListener('click', delegationFunc);
 window.addEventListener('scroll', scrollFunc);
-
